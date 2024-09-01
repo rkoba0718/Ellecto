11d7842f351291dc3327712381f9163031f87ea7
小林亮太 <ahvdbdndjjf@gmail.com>
2024-09-01 11:08:34 +0900

    Modify global layout to add header and footer

---
+++ visualize-tool/src/app/(components)/containers/Header/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Header from "../../presentationals/Header";
+
+const HeaderContainer = () => {
+    return (
+        <Header />
+    )
+};
+
+export default HeaderContainer;

f78e6de3ca2c3afbbf2dc395663c3f018205d473
小林亮太 <ahvdbdndjjf@gmail.com>
2024-09-02 17:37:51 +0900

    Add footer component

--- visualize-tool/src/app/(components)/presentationals/Footer/index.tsx
+++ visualize-tool/src/app/(components)/presentationals/Footer/index.tsx
@@ -1,5 +1,12 @@
 export default function Header() {
     return (
-        <div>footer</div>
+        <footer className="bg-gray-800 bg-opacity-75 text-white py-4">
+            <div className="container mx-auto text-center">
+                <p className="font-bold">HIGO LAB</p>
+                <p>Department of Computer Science</p>
+                <p>Graduate School of Information Science and Technology</p>
+                <p>Osaka University</p>
+            </div>
+        </footer>
     )
 }

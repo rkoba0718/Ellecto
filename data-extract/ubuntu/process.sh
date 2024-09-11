0fadb8d5e193e25a2301303c240930bb3ad2b9c7
小林亮太 <ahvdbdndjjf@gmail.com>
2024-09-11 18:33:55 +0900

    Add shell script process to create and copy input files for crosssim

--- data-extract/ubuntu/process.sh
+++ data-extract/ubuntu/process.sh
@@ -15,3 +15,7 @@ do
         node dbimport.js controlDatas/$PACKAGE_NAME.json;
     fi
 done < packages.txt
+node createCrossSimInputFiles.js;
+cp RepositoryList.txt /shared/;
+cp Dictionary.txt /shared/;
+cp Graph.txt /shared/;

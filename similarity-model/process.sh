c6d35914384dc7c986cf46fef5e07ea1b06ccd13
小林亮太 <ahvdbdndjjf@gmail.com>
2024-09-12 17:01:45 +0900

    Add crosssim and dbimport script

---
+++ similarity-model/process.sh
@@ -0,0 +1,9 @@
+cd Inputs;
+rm -rf *;
+cd ..;
+cd Results;
+rm -rf *;
+cd ..;
+cp ../shared/* Inputs;
+mvn -e exec:java -Dexec.mainClass="org.crossminer.similaritycalculator.CrossSim.Runner";
+mvn -e exec:java -Dexec.mainClass="org.importor.MongoDataInsert";

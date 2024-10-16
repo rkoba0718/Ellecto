0443b0bf43bbeb57d5a6d33caa937a10212f6d01
小林亮太 <ahvdbdndjjf@gmail.com>
2024-10-16 15:27:22 +0900

    Add page link in Header

--- visualize-tool/src/app/(components)/presentationals/Header/index.tsx
+++ visualize-tool/src/app/(components)/presentationals/Header/index.tsx
@@ -1,5 +1,27 @@
+"use client";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInfoCircle, faPlusCircle } from '@fortawesome/free-solid-svg-icons';
+
 export default function Header() {
     return (
-        <div>header</div>
+        <header className="bg-green-500 text-white shadow-md">
+            <nav className="container mx-auto py-2 flex justify-between items-center">
+                <div className="text-lg font-bold">
+                    <a href="/" className="hover:text-gray-200">ProjSelector</a>
+                </div>
+
+                <div>
+                    <a href="/about" className="hover:text-gray-200 pr-2.5">
+                        <FontAwesomeIcon icon={faInfoCircle} className="pr-1" />
+                        About
+                    </a>
+                    <a href="/add" className="hover:text-gray-200">
+                        <FontAwesomeIcon icon={faPlusCircle} className="pr-1" />
+                        Add
+                    </a>
+                </div>
+            </nav>
+        </header>
     )
 }

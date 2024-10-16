e853aabc2adaf1d45c17c3b54b78650bb8643513
小林亮太 <ahvdbdndjjf@gmail.com>
2024-10-16 15:54:45 +0900

    Add use client

--- visualize-tool/src/app/(components)/presentationals/Footer/index.tsx
+++ visualize-tool/src/app/(components)/presentationals/Footer/index.tsx
@@ -1,4 +1,6 @@
-export default function Header() {
+"use client";
+
+export default function Footer() {
     return (
         <footer className="bg-gray-800 bg-opacity-75 text-white py-4">
             <div className="container mx-auto text-center">

cd34ff1aaecbbed4acf62610e6f627e1dff420ff
小林亮太 <ahvdbdndjjf@gmail.com>
2024-10-30 12:30:43 +0900

    Modify ProjectCard

--- visualize-tool/src/app/(components)/presentationals/projects/ProjectCard/index.tsx
+++ visualize-tool/src/app/(components)/presentationals/projects/ProjectCard/index.tsx
@@ -1,8 +1,51 @@
 "use client";
 
 import React from 'react';
+import Link from 'next/link';
 
 import { ProjectInfo } from '@/app/types/ProjectInfo';
+
+type SummaryViewProps = {
+    project: ProjectInfo;
+};
+
+const SummaryView: React.FC<SummaryViewProps> = ({
+    project,
+}) => {
+    const summaries = Object.keys(project.Package)
+        .map((pkgKey) => project.Package[pkgKey].Description?.summary)
+        .filter((s) => s !== undefined);
+    const length = summaries.length;
+
+    return (
+        <div className="ml-2">
+            {length === 0 ? (
+                <>{project.Description.summary}</>
+            ) :
+            length === 1 ? (
+                <>{summaries[0]}</>
+            ) :
+            length <= 3 ? (
+                <ul className="list-disc pl-4">
+                    {summaries.map((summary, index) => (
+                        <li key={index}>{summary}</li>
+                    ))}
+                </ul>
+            ) : (
+                <>
+                    <ul className="list-disc pl-4">
+                        {summaries.slice(0, 3).map((summary, index) => (
+                            <li key={index}>{summary}</li>
+                        ))}
+                    </ul>
+                    <Link href={`/projects/${project._id}`} className="text-blue-500 hover:underline ml-5">
+                        ...more
+                    </Link>
+                </>
+            )}
+        </div>
+    );
+};
 
 type ProjectCardProps = {
   project: ProjectInfo;
@@ -10,21 +53,21 @@ type ProjectCardProps = {
 
 const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
     return (
-        <div className="flex border rounded-lg p-4 mb-6 shadow-lg">
-            <div className="w-4/5">
+        <div className="flex border rounded-lg p-5 mb-6 shadow-lg">
+            <div className="w-4/5 ml-2">
                 <h2 className="text-xl font-bold mb-2">
-                    <a href={`/projects/${project._id}`} className="text-blue-500 hover:underline">
+                    <Link href={`/projects/${project._id}`} className="text-blue-500 hover:underline">
                         {project.Name}
-                    </a>
+                    </Link>
                 </h2>
                 <p className="text-gray-700 mb-4">
-                    {project.Description.summary}
+                    <SummaryView project={project} />
                 </p>
             </div>
 
-            <div className="w-1/5 flex flex-col justify-between">
-                {project.Section && <p><strong>Section:</strong> {project.Section}</p>}
-                {project.Version['Standards-Version'] && <p><strong>Version:</strong> {project.Version['Standards-Version']}</p>}
+            <div className="w-1/5 flex flex-col justify-center">
+                {project.Section && <p className="mb-1"><strong>Section:</strong> {project.Section}</p>}
+                <p className="mb-1"><strong>Language:</strong> {project.Language.Lang1.Name}</p>
                 <p><strong>License:</strong> {project.License}</p>
             </div>
         </div>

b4a9ebc4b65fb78183e10f4ded78bef1da9a5084
小林亮太 <ahvdbdndjjf@gmail.com>
2024-10-31 14:42:15 +0900

    Add title in LanguageInfo

--- visualize-tool/src/app/(components)/presentationals/projects/shows/LanguageInfo/index.tsx
+++ visualize-tool/src/app/(components)/presentationals/projects/shows/LanguageInfo/index.tsx
@@ -17,42 +17,45 @@ const LanguageInfo: React.FC<LanguageInfoProps> = ({
     loc
 }) => {
     return (
-        <div className="flex items-center">
-            <div className="mr-3">
-                <PieChart width={250} height={200}>
-                    <Pie
-                        data={data}
-                        cx="60%"
-                        cy="50%"
-                        innerRadius={40}
-                        outerRadius={65}
-                        startAngle={90}
-                        endAngle={-270}
-                        fill="#8884d8"
-                        dataKey="percentage"
-                    >
+        <>
+            <h2 className="font-bold text-xl mb-2">Code</h2>
+            <div className="flex items-center">
+                <div>
+                    <PieChart width={200} height={200}>
+                        <Pie
+                            data={data}
+                            cx="50%"
+                            cy="50%"
+                            innerRadius={40}
+                            outerRadius={65}
+                            startAngle={90}
+                            endAngle={-270}
+                            fill="#8884d8"
+                            dataKey="percentage"
+                        >
+                            {data.map((entry, index) => (
+                                <Cell key={`cell-${index}`} fill={entry.color} />
+                            ))}
+                        </Pie>
+                    </PieChart>
+                </div>
+                <div className="mt-6">
+                    <ul className="space-y-1">
                         {data.map((entry, index) => (
-                            <Cell key={`cell-${index}`} fill={entry.color} />
+                            <li key={index} className="flex items-center space-x-2">
+                                <div
+                                    className="w-3 h-3 rounded-full"
+                                    style={{ backgroundColor: entry.color }}
+                                    />
+                                <span>{entry.name}</span>
+                                <span>{entry.percentage}%</span>
+                            </li>
                         ))}
-                    </Pie>
-                </PieChart>
+                    </ul>
+                    <h3 className="mt-3"><strong>LOC:</strong> {loc}</h3>
+                </div>
             </div>
-            <div className="mt-6">
-                <ul className="space-y-1">
-                    {data.map((entry, index) => (
-                        <li key={index} className="flex items-center space-x-2">
-                            <div
-                                className="w-3 h-3 rounded-full"
-                                style={{ backgroundColor: entry.color }}
-                            />
-                            <span>{entry.name}</span>
-                            <span>{entry.percentage}%</span>
-                        </li>
-                    ))}
-                </ul>
-                <h3 className="mt-3"><strong>LOC:</strong> {loc}</h3>
-            </div>
-        </div>
+        </>
     );
 };
 

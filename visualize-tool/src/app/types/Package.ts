577471eacce90582707057df10a23f39182064b9
小林亮太 <ahvdbdndjjf@gmail.com>
2024-11-02 09:55:36 +0900

    Add Architecture in Package type

--- visualize-tool/src/app/types/Package.ts
+++ visualize-tool/src/app/types/Package.ts
@@ -3,6 +3,7 @@ import { Description } from "./Description";
 export type Package = {
     [key: string]: {
         Name: string;
+        Architecture: string;
         Description?: Description;
         [key: string]: any;
     };

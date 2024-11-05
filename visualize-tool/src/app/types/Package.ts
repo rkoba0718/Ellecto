8ae272d02bbf49ce8dfe33b2b4e5b5fe728e7249
小林亮太 <ahvdbdndjjf@gmail.com>
2024-11-05 11:46:00 +0900

    Add Depends in Package type

--- visualize-tool/src/app/types/Package.ts
+++ visualize-tool/src/app/types/Package.ts
@@ -1,10 +1,12 @@
 import { Description } from "./Description";
+import { Dependency } from "./Dependency";
 
 export type Package = {
     [key: string]: {
         Name: string;
         Architecture: string;
         Description?: Description;
+        Depends?: Dependency[];
         [key: string]: any;
     };
 };

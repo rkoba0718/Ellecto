f3c829d04366e4ec81f18771e9b2a10e23249dd6
rkoba0718 <ahvdbdndjjf@gmail.com>
2024-12-26 16:14:58 +0900

    Modify description summary view in projects page

--- visualize-tool/src/app/(components)/presentationals/projects/ProjectCard/index.tsx
+++ visualize-tool/src/app/(components)/presentationals/projects/ProjectCard/index.tsx
@@ -5,48 +5,6 @@ import Link from 'next/link';
 
 import { ProjectInfo } from '@/app/types/ProjectInfo';
 
-type SummaryViewProps = {
-    project: ProjectInfo;
-};
-
-const SummaryView: React.FC<SummaryViewProps> = ({
-    project,
-}) => {
-    const summaries = Object.keys(project.Package)
-        .map((pkgKey) => project.Package[pkgKey].Description?.summary)
-        .filter((s) => s !== undefined);
-    const length = summaries.length;
-
-    return (
-        <div className="ml-2">
-            {length === 0 ? (
-                <>{project.Description.summary}</>
-            ) :
-            length === 1 ? (
-                <>{summaries[0]}</>
-            ) :
-            length <= 3 ? (
-                <ul className="list-disc pl-4">
-                    {summaries.map((summary, index) => (
-                        <li key={index}>{summary}</li>
-                    ))}
-                </ul>
-            ) : (
-                <>
-                    <ul className="list-disc pl-4">
-                        {summaries.slice(0, 3).map((summary, index) => (
-                            <li key={index}>{summary}</li>
-                        ))}
-                    </ul>
-                    <Link href={`/projects/${project._id}`} className="text-blue-500 hover:underline ml-5">
-                        ...more
-                    </Link>
-                </>
-            )}
-        </div>
-    );
-};
-
 type ProjectCardProps = {
   project: ProjectInfo;
 }
@@ -61,7 +19,9 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
                     </Link>
                 </h2>
                 <div className="text-gray-700 mb-4">
-                    <SummaryView project={project} />
+                    <div className="ml-2">
+                        {project.Description.summary}
+                    </div>
                 </div>
             </div>
 
